51e3c7d254b2ae8016b2422945ccea5de9095e84
Ivan Svidinskiy <icommerce777@gmail.com>
2025-11-21 01:54:21 +0300

    feat: lesson 5 hw

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,36 @@
-export default function RootLayout({children,}: Readonly<{
+import localFont from 'next/font/local'
+import Header from './ui/header';
+import Cart from './ui/cart';
+
+const GTEestiProText = localFont({
+  src: [
+    {
+      path: './fonts/EestiBold.otf',
+      weight: '700',
+    },
+    {
+      path: './fonts/EestiRegular.otf',
+      weight: '500',
+    },
+  ],
+})
+
+
+export default function RootLayout({ children, }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body>
-        {children}
+      <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" />
+      </head>
+      <body className={GTEestiProText.className}>
+        <Header />
+        <main>
+          {children}
+        </main>
+        <Cart />
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,61 @@
 import Link from "next/link";
+import './scss/style.scss'
 
 export default function Home() {
   return (
     <>
-      <strong>Hello</strong><br/>
-      <Link href="/secondpage">Second Page</Link>
+      <div className="container">
+        <div className="row">
+          <div className="col-3 col-xl-2 d-none d-lg-block">
+            <div className="filter">
+              <div className="filter-title">
+                <h5>Фильтр</h5>
+              </div>
+              <div className="filter-price">
+                <div className="filter-price_title">
+                  Цена
+                </div>
+                <form>
+                  <div className="filter-price_range">
+                    <div className="filter-price_input-wrapper">
+                      <label htmlFor="min" className="filter-price_label">от</label>
+                      <input id="min" className="filter-price_input" />
+                    </div>
+                    <div className="filter-price_input-wrapper">
+                      <label htmlFor="max" className="filter-price_label">до</label>
+                      <input id="max" className="filter-price_input" /></div>
+                  </div>
+                </form>
+              </div>
+              <div className="filter-check">
+                <label className="filter-check_label">
+                  <input type="checkbox" className="filter-check_checkbox" id="discount-checkbox" />
+                  <span className="filter-check_checkmark"></span>
+                  <span className="filter-check_label-text">Акция</span>
+                </label>
+              </div>
+              <div>
+                <ul>
+                  <li><a id="postRequest" href="#">POST-запрос</a></li>
+                  <li><a id="putRequest" href="#">PUT-запрос</a></li>
+                  <li><a id="patchRequest" href="#">PATCH-запрос</a></li>
+                  <li><a id="deleteRequest" href="#">DELETE-запрос</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-lg-9 col-xl-10">
+            <div className="container">
+              <div className="row no-gutters goods">
+                {/* <!-- КАРТОЧКИ ТОВАРОВ --> */}
+
+                {/* <!-- END КАРТОЧКИ ТОВАРОВ --> */}
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </>
   );
 }
